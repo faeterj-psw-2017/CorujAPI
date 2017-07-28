8e5f9c3845a46069771fe09b89049651eabd723a
Marco Aurélio da Silva <marcoonroad@gmail.com>
2017-07-28 06:07:04 -0300

    Alterando arquivos do Bruno.

--- src/main/java/org/faeterj/apicoruja/coruja/service/AtcomService.java
+++ src/main/java/org/faeterj/apicoruja/coruja/service/AtcomService.java
@@ -5,9 +5,11 @@ import org.faeterj.apicoruja.coruja.model.repository.ATCOMRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.ArrayList;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AtcomService {
-
 
     private ATCOMRepository atcomRepositorio;
 
@@ -20,17 +22,31 @@ public class AtcomService {
     public void init ( ) {
     }
 
-
-    public ATCOM create (ATCOM atcom) { return atcomRepositorio.save (atcom);    }
-
-    public  void delete(Long id){atcomRepositorio.delete(id);}
-
-    public List<ATCOM> listarAtcom ( ) {
-        return (List<ATCOM>) atcomRepositorio.findAll ( );
+    public ATCOM create (ATCOM atcom) {
+        return atcomRepositorio.save (atcom);
     }
 
-    public ATCOM encontrarAtcomPeloId (Long id) {return atcomRepositorio.findById (id); }
+    public void delete (Long id) {
+        atcomRepositorio.delete (id);
+    }
 
-    public ATCOM atualiza(ATCOM atcom){return atcomRepositorio.update(atcom);}
+    public List<ATCOM> listarAtcom ( ) {
+        Iterable<ATCOM> iterable = atcomRepositorio.findAll ( );
+        List<ATCOM> list = new ArrayList<ATCOM> ( );
+
+        for (ATCOM atcom : iterable) {
+            list.add (atcom);
+        }
+
+        return list;
+    }
+
+    public ATCOM encontrarAtcomPeloId (Long id) {
+        return atcomRepositorio.findOne (id);
+    }
+
+    public ATCOM atualiza (ATCOM atcom) {
+        return atcomRepositorio.save (atcom);
+    }
 
 }
